7d78737f899d50dee23753aa85d2f1ba3cee9944
Victor Lee <victorlee0505@gmail.com>
2021-06-23 13:08:58 -0400

    TotpConstant: update for Encryptor

--- src/main/java/com/example/security/constants/TotpConstant.java
+++ src/main/java/com/example/security/constants/TotpConstant.java
@@ -3,5 +3,9 @@ package com.example.security.constants;
 public class TotpConstant {
 
     public static final String PNG = "png";
-    
+
+    public static final String CIPHER_ALGO = "AES";
+    public static final String CIPHER_SPEC = CIPHER_ALGO + "/CBC/PKCS5Padding";
+
+    public static final int KEY_SIZE = 256;
 }
